75a326440d7cd201fb8c65c8d87c2bda58c228d9
Giv Parvaneh <giv@givp.org>
2022-10-03 13:48:31 -0700

    Cleanup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is the tutorial from [DApp University](https://www.dappuniversity.com/) which is configured to work on harmony blockchain.
+This is the tutorial from [DApp University](https://www.dappuniversity.com/) which is configured to work on Findora blockchain.
 
 Steps
 * `npm install`
--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,11 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    testnet: {
+      host: "https://prod-testnet.prod.findora.org",
+      port: 8545,
+      network_id: 2153
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
